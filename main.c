939e38da329bf8dd04ac61000ccc7b8c6c9a12b7
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2022-10-04 17:05:26 +0900

    03

--- main.c
+++ main.c
@@ -4,15 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-
-	printf("정수 하나를 입력하십시오:");
-	scanf("%d", &x);
-
-	if (x<0)
-   		printf("절대값은 %i 입니다", -x);
-    else if (x>0)
-      	printf("절대값은 %i 입니다", x);
+	char c;
+	int num; num = 0;
+	printf("input a string:");
+	while((c= getchar()) != '\n') //입력 문자가 개행문자가 나올 때까지 반복 
+	{
+		if (c >= '0' && c <= '9' ) //숫자는 '0'이상 '9'이 하 
+			num = num + 1;
+		
+	}
+	
+	printf("the number of digits is %d", num);
+	 
 
 	return 0;
 }
