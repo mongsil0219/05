2734e09f544d6b7991f7a0a42c55c03c6804b66d
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2022-10-04 17:44:26 +0900

    06

--- main.c
+++ main.c
@@ -4,22 +4,35 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int x, answer;
+	answer = 59;
+	int trial = 0; 
+	do
+	{	
+		printf("Guess a number:");
+		scanf("%d", &x);
+		//입력받은 숟자가 정답보다 큰지 작은지 출력
+		 
+		if  ( x < answer)
+		{
+			printf("Low\n");
+			trial = trial + 1;
+		}		
+		else if ( x > answer)
+		{
+			printf("High\n");
+			trial = trial + 1;
+		}
+			
+		else if ( x == answer)
+		{
+			trial = trial + 1;
+			break;
+		}
+			
+	} while (x != answer); //정답과 같은지 여부 판 단 
 	
-	int x,y,result;
-	char z;
-	printf("enter the calculation:");
-	scanf("%d%c%d", &x,&z,&y);
-	
-	if (z == '+')
-		result = x+y;
-	else if (z == '-')
-		result = x-y;
-	else if (z == '*')
-		result = x*y;
-	else if ( z == '/')
-		result = x/y;
-		
-	printf("= %d", result);
+	printf("Congratulation! trials : %d", trial);
  
 
 	return 0;
